9c97d2483b50fe5041e0a33540ce3c72563f2914
Samuel Vidovich <samuel.vidovich@gmail.com>
2017-12-19 00:52:16 -0500

    Upload a sample

---
+++ generator10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+int main()
+{
+ofstream myfile;
+myfile.open("255x255v1333.html");
+const double c = 1;
+
+myfile << "<html>\n<head></head>\n<body>\n<table width=100%\"\">";
+for(int i=0; i<=255; i++)
+{
+	myfile << "<tr>\n";
+	for(int j=0; j<=255; j++)
+	{
+		double a, b;
+		myfile << "<td cellpadding=\"1px\" cellspacing=\"0\" bgcolor = \"rgb("; 
+		myfile << (static_cast<int>((pow(i, 2) - pow(j, 2))/c)%255) << ","; 
+		myfile << (static_cast<int>((pow(i,2) - pow(j,2))/c)%255); 
+		myfile << ",0)\">.</td>\n";
+
+	}
+	myfile << "</tr>\n";
+}
+myfile << "\n</table>\n</body>\n</html>\n";
+myfile.close();
+return 0;
+}
